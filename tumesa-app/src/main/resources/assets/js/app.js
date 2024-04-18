ce06cdc6c24e7408061b8384f42cea25154befcf
alvaro-ortiz-fernandez <40672795+alvaro-ortiz-fernandez@users.noreply.github.com>
2024-04-18 22:48:40 +0200

    Dejando funcionando lo básico de front

--- tumesa-app/src/main/resources/assets/js/app.js
+++ tumesa-app/src/main/resources/assets/js/app.js
@@ -6,7 +6,7 @@ axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8';
 let app = {
     data() {
         return {
-            hello: "Hello world"
+            appPath: appPath
         }
     }
 };
@@ -14,14 +14,18 @@ let app = {
 let router = VueRouter.createRouter({
     history: VueRouter.createMemoryHistory(),
     routes: [
+        { path: '/', component: loginComponent },
         { path: '/login', component: loginComponent },
-        { path: '/chat', component: {} }
+        { path: '/signup', component: signupComponent }
     ],
 });
 
 addEventListener("DOMContentLoaded", (event) => {
     Vue.createApp(app)
        .use(router)
+       .component('app-header', appHeaderComponent)
+       .component('app-footer', appFooterComponent)
        .component('login', loginComponent)
+       .component('signup', signupComponent)
        .mount('#app');
 });
