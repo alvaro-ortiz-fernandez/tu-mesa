60a345740beca7d9c9244bab2e9cc81ed7a92dc8
alvaro-ortiz-fernandez <40672795+alvaro-ortiz-fernandez@users.noreply.github.com>
2024-05-25 02:38:50 +0200

    Cambiando la obtención del config.properties

--- src/main/java/com/uoc/tumesa/app/spring/config/components/PropertiesConfig.java
+++ src/main/java/com/uoc/tumesa/app/spring/config/components/PropertiesConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Clase de configuración de Spring Boot, con los beans que obtienen la configuración en properties del despliegue.
@@ -16,7 +16,28 @@ public class PropertiesConfig {
 
     @Bean(name = "configManager")
     public ConfigManager configManager() throws IOException {
-        URL configUrl = Objects.requireNonNull(this.getClass().getResource("/config.properties"));
+
+        URL configUrl = getConfigFromClasspath()
+                .orElseGet(() -> getConfigFromClassloader()
+                .orElseThrow(() -> new IllegalStateException(
+                        "No se encontró el archivo config.properties en el classpath ni en el classloader")));
+
         return new ConfigManager(configUrl);
+    }
+
+    /**
+     * Método que busca el fichero 'config.properties' en el classpath.
+     * Es la forma en que podemos cargar este fichero cuando ejecutamos la aplicación en un IDE en local.
+     * */
+    protected Optional<URL> getConfigFromClasspath() {
+        return Optional.ofNullable(this.getClass().getResource("/config.properties"));
+    }
+
+    /**
+     * Método que busca el fichero 'config.properties' en el classloader.
+     * Es la forma en que podemos cargar este fichero cuando desplegamos la aplicación en un servidor.
+     * */
+    protected Optional<URL> getConfigFromClassloader() {
+        return Optional.ofNullable(this.getClass().getClassLoader().getResource("config.properties"));
     }
 }
